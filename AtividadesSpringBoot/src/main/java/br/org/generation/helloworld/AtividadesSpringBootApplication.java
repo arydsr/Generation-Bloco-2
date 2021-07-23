c3b774ca59d71027a060f95da5bf93a0c170764d
Ary de Sá <souoary@gmail.com>
2021-07-23 12:55:05 -0300

    Atividades Spring-Boot | 23-07-2021

---
+++ AtividadesSpringBoot/src/main/java/br/org/generation/helloworld/AtividadesSpringBootApplication.java
@@ -0,0 +1,13 @@
+package br.org.generation.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadesSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtividadesSpringBootApplication.class, args);
+	}
+
+}
---
+++ AtividadesSpringBoot/src/main/java/br/org/generation/helloworld/controller/HelloController.java
@@ -0,0 +1,27 @@
+package br.org.generation.helloworld.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/atividade1") //Quando digitar no navegador essa classe deve responder ao meu navegador o endereço digitado
+public class HelloController {
+	
+	@GetMapping //responder requisião do tipo Mapping, requisição de consulta
+	public String MentalidadeHabilidade() {
+		return "Mentalidade: Responsabilidade Pessoal | Habilidade: Atenção aos Detalhes"; //Metodo do tipo String que Retorna o valor da String
+	}
+	
+	@RequestMapping("/atividade2")
+	public String retornaObjetivos() {
+		return "Tenho o objetivo de continuar aumentando a minha porcentagem de aproveitamento do bootcamp"
+				+ ", fazer alguns cursos do Alura e Udemy dentro do que estou aprendendo e alguns"
+				+ " exercícios extras para fixar o aprendizado. Entrar menos em pânico como no primeiro bloco e"
+				+ " focar em encontrar soluções para as minhas dificuldades. ";
+	}
+	
+}
+
+
+
